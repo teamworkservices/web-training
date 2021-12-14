479e41ea51c9702c7517d721b4c176b28b9eeb9e
Alex A. García Potosí <agarcia@kpitec.com>
2021-12-14 14:32:15 -0500

    proyecto_inicial_Alex
    Se sube el proyecto de capacitación de frontend de Alex

---
+++ farmacia-alex/src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout/layout.component';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
